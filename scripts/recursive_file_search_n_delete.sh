b814d55533a15bbca173151c827759a15c829a63
Ketema Harris <ketema@ketema.net>
2010-06-04 02:15:02 -0400

    15 minute auto commit

--- scripts/recursive_file_search_n_delete.sh
+++ scripts/recursive_file_search_n_delete.sh
@@ -10,8 +10,6 @@ fi
 
 cd ${1}
 SEARCH=${2} 
-#		$(/some/file/$(grep -rh "${SEARCH}" *  < execute the file we find the pattern in? WRONG... must be an older script
-#		$(ls -1 /some/file/$(grep -rh "blah blah <- full path				
 for EACH in $(echo ${1}/$(grep -rH "${SEARCH}" * | cut -f1 -d":")); do
 	#echo "sed -i \">/${SEARCH}/d<\" ${EACH}"
 	sed -i "/${SEARCH}/d" ${EACH}
