09635e114c83144f97d272646db540cd08e26cc0
Ketema Harris <ketema@ketema.net>
2010-06-04 00:09:59 -0400

    Added recursive file search and delete

---
+++ scripts/recursive_file_search_n_delete.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#It is the purpose of this script to loop over all the files in a directory
+#and the files in subdirectories looking for a pattern and deleting it
+
+if [ -z "${2}" ]; then
+	echo "really? try using $(basename ${0}) direcory search_pattern  dummy"
+	exit
+fi
+
+cd ${1}
+SEARCH=${2}
+
+for EACH in $(${1}/$(grep -rH "${SEARCH}" * | cut -f1 -d":")); do
+	echo "sed -i \"/${SEARCH}/d\" ${EACH}"
+	#sed -i "/${SEARCH}/d" ${EACH};
+done
